b41f911d42ffa14c0cdc3622b86d54cc99420cb2
aryav <104564110+aryav-hash@users.noreply.github.com>
2025-06-17 16:57:28 +0530

    feat: The client side of the project is also working.

--- finnhub/client/src/App.jsx
+++ finnhub/client/src/App.jsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 function App() {
   return (
     <>
-      
+      <div>
+        <h1 className="flex justify-center"> This is the finnhub project! </h1>
+      </div>
     </>
   )
 }
